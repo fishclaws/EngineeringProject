c6378be01dbb5cfdad273e288a0463a9ab180dc5
Sam Faynzilberg <Sam Faynzilberg>
2016-03-02 17:16:13 -0500

    Login bug-fixes and base url redirect

--- EngineeringProject/urls.py
+++ EngineeringProject/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     url(r'^feature_requests/', include('feature_requests.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', lambda r: HttpResponseRedirect('feature_requests/'))
 ]
